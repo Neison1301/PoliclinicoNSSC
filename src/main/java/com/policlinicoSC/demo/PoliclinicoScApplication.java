8cea2405877c89db0b424a5d3bc2ff2d3f933d83
Neison1301 <neisonssj2@gmail.com>
2025-09-16 15:53:42 -0500

    inicio del proyecto

---
+++ src/main/java/com/policlinicoSC/demo/PoliclinicoScApplication.java
@@ -0,0 +1,13 @@
+package com.policlinicoSC.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliclinicoScApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliclinicoScApplication.class, args);
+	}
+
+}
